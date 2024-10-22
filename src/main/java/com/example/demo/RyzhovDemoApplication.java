ee9ffde4f2f26369c1dfc1990586d8eab04e16c2
NikitaRyzhov1 <ryzhovnikita2017@gmail.com>
2024-10-22 12:26:29 +0300

    init

---
+++ src/main/java/com/example/demo/RyzhovDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyzhovDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyzhovDemoApplication.class, args);
+	}
+
+}
